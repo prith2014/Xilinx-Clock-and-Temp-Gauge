51d52e22468cf6733366b6728503502ffe42d89d
Prithvi Venkat <prith2014@gmail.com>
2020-03-18 16:33:09 -0400

    Initial Commit

---
+++ Final_Project_RTCC_TMP3.sdk/RTCC_OLED_TMP/Debug/src/main.d
@@ -0,0 +1,52 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/PmodTMP3.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xiic.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xiic_l.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/PmodTMP3.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xiic.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xiic_l.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../RTCC_OLED_TMP_bsp/ps7_cortexa9_0/include/xparameters.h:
---
+++ Final_Project_RTCC_TMP3.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,485 @@
+//Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2016.4 (win64) Build 1756540 Mon Jan 23 19:11:23 MST 2017
+//Date        : Thu Dec 14 03:20:24 2017
+//Host        : DESKTOP-CT1471N running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    btns_4bits_tri_i,
+    ja_pin10_io,
+    ja_pin1_io,
+    ja_pin2_io,
+    ja_pin3_io,
+    ja_pin4_io,
+    ja_pin7_io,
+    ja_pin8_io,
+    ja_pin9_io,
+    jb_pin10_io,
+    jb_pin1_io,
+    jb_pin2_io,
+    jb_pin3_io,
+    jb_pin4_io,
+    jb_pin7_io,
+    jb_pin8_io,
+    jb_pin9_io,
+    jc_pin10_io,
+    jc_pin1_io,
+    jc_pin2_io,
+    jc_pin3_io,
+    jc_pin4_io,
+    jc_pin7_io,
+    jc_pin8_io,
+    jc_pin9_io,
+    leds_4bits_tri_io,
+    sws_4bits_tri_i);
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  input [3:0]btns_4bits_tri_i;
+  inout ja_pin10_io;
+  inout ja_pin1_io;
+  inout ja_pin2_io;
+  inout ja_pin3_io;
+  inout ja_pin4_io;
+  inout ja_pin7_io;
+  inout ja_pin8_io;
+  inout ja_pin9_io;
+  inout jb_pin10_io;
+  inout jb_pin1_io;
+  inout jb_pin2_io;
+  inout jb_pin3_io;
+  inout jb_pin4_io;
+  inout jb_pin7_io;
+  inout jb_pin8_io;
+  inout jb_pin9_io;
+  inout jc_pin10_io;
+  inout jc_pin1_io;
+  inout jc_pin2_io;
+  inout jc_pin3_io;
+  inout jc_pin4_io;
+  inout jc_pin7_io;
+  inout jc_pin8_io;
+  inout jc_pin9_io;
+  inout [3:0]leds_4bits_tri_io;
+  input [3:0]sws_4bits_tri_i;
+
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire [3:0]btns_4bits_tri_i;
+  wire ja_pin10_i;
+  wire ja_pin10_io;
+  wire ja_pin10_o;
+  wire ja_pin10_t;
+  wire ja_pin1_i;
+  wire ja_pin1_io;
+  wire ja_pin1_o;
+  wire ja_pin1_t;
+  wire ja_pin2_i;
+  wire ja_pin2_io;
+  wire ja_pin2_o;
+  wire ja_pin2_t;
+  wire ja_pin3_i;
+  wire ja_pin3_io;
+  wire ja_pin3_o;
+  wire ja_pin3_t;
+  wire ja_pin4_i;
+  wire ja_pin4_io;
+  wire ja_pin4_o;
+  wire ja_pin4_t;
+  wire ja_pin7_i;
+  wire ja_pin7_io;
+  wire ja_pin7_o;
+  wire ja_pin7_t;
+  wire ja_pin8_i;
+  wire ja_pin8_io;
+  wire ja_pin8_o;
+  wire ja_pin8_t;
+  wire ja_pin9_i;
+  wire ja_pin9_io;
+  wire ja_pin9_o;
+  wire ja_pin9_t;
+  wire jb_pin10_i;
+  wire jb_pin10_io;
+  wire jb_pin10_o;
+  wire jb_pin10_t;
+  wire jb_pin1_i;
+  wire jb_pin1_io;
+  wire jb_pin1_o;
+  wire jb_pin1_t;
+  wire jb_pin2_i;
+  wire jb_pin2_io;
+  wire jb_pin2_o;
+  wire jb_pin2_t;
+  wire jb_pin3_i;
+  wire jb_pin3_io;
+  wire jb_pin3_o;
+  wire jb_pin3_t;
+  wire jb_pin4_i;
+  wire jb_pin4_io;
+  wire jb_pin4_o;
+  wire jb_pin4_t;
+  wire jb_pin7_i;
+  wire jb_pin7_io;
+  wire jb_pin7_o;
+  wire jb_pin7_t;
+  wire jb_pin8_i;
+  wire jb_pin8_io;
+  wire jb_pin8_o;
+  wire jb_pin8_t;
+  wire jb_pin9_i;
+  wire jb_pin9_io;
+  wire jb_pin9_o;
+  wire jb_pin9_t;
+  wire jc_pin10_i;
+  wire jc_pin10_io;
+  wire jc_pin10_o;
+  wire jc_pin10_t;
+  wire jc_pin1_i;
+  wire jc_pin1_io;
+  wire jc_pin1_o;
+  wire jc_pin1_t;
+  wire jc_pin2_i;
+  wire jc_pin2_io;
+  wire jc_pin2_o;
+  wire jc_pin2_t;
+  wire jc_pin3_i;
+  wire jc_pin3_io;
+  wire jc_pin3_o;
+  wire jc_pin3_t;
+  wire jc_pin4_i;
+  wire jc_pin4_io;
+  wire jc_pin4_o;
+  wire jc_pin4_t;
+  wire jc_pin7_i;
+  wire jc_pin7_io;
+  wire jc_pin7_o;
+  wire jc_pin7_t;
+  wire jc_pin8_i;
+  wire jc_pin8_io;
+  wire jc_pin8_o;
+  wire jc_pin8_t;
+  wire jc_pin9_i;
+  wire jc_pin9_io;
+  wire jc_pin9_o;
+  wire jc_pin9_t;
+  wire [0:0]leds_4bits_tri_i_0;
+  wire [1:1]leds_4bits_tri_i_1;
+  wire [2:2]leds_4bits_tri_i_2;
+  wire [3:3]leds_4bits_tri_i_3;
+  wire [0:0]leds_4bits_tri_io_0;
+  wire [1:1]leds_4bits_tri_io_1;
+  wire [2:2]leds_4bits_tri_io_2;
+  wire [3:3]leds_4bits_tri_io_3;
+  wire [0:0]leds_4bits_tri_o_0;
+  wire [1:1]leds_4bits_tri_o_1;
+  wire [2:2]leds_4bits_tri_o_2;
+  wire [3:3]leds_4bits_tri_o_3;
+  wire [0:0]leds_4bits_tri_t_0;
+  wire [1:1]leds_4bits_tri_t_1;
+  wire [2:2]leds_4bits_tri_t_2;
+  wire [3:3]leds_4bits_tri_t_3;
+  wire [3:0]sws_4bits_tri_i;
+
+  design_1 design_1_i
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .btns_4bits_tri_i(btns_4bits_tri_i),
+        .ja_pin10_i(ja_pin10_i),
+        .ja_pin10_o(ja_pin10_o),
+        .ja_pin10_t(ja_pin10_t),
+        .ja_pin1_i(ja_pin1_i),
+        .ja_pin1_o(ja_pin1_o),
+        .ja_pin1_t(ja_pin1_t),
+        .ja_pin2_i(ja_pin2_i),
+        .ja_pin2_o(ja_pin2_o),
+        .ja_pin2_t(ja_pin2_t),
+        .ja_pin3_i(ja_pin3_i),
+        .ja_pin3_o(ja_pin3_o),
+        .ja_pin3_t(ja_pin3_t),
+        .ja_pin4_i(ja_pin4_i),
+        .ja_pin4_o(ja_pin4_o),
+        .ja_pin4_t(ja_pin4_t),
+        .ja_pin7_i(ja_pin7_i),
+        .ja_pin7_o(ja_pin7_o),
+        .ja_pin7_t(ja_pin7_t),
+        .ja_pin8_i(ja_pin8_i),
+        .ja_pin8_o(ja_pin8_o),
+        .ja_pin8_t(ja_pin8_t),
+        .ja_pin9_i(ja_pin9_i),
+        .ja_pin9_o(ja_pin9_o),
+        .ja_pin9_t(ja_pin9_t),
+        .jb_pin10_i(jb_pin10_i),
+        .jb_pin10_o(jb_pin10_o),
+        .jb_pin10_t(jb_pin10_t),
+        .jb_pin1_i(jb_pin1_i),
+        .jb_pin1_o(jb_pin1_o),
+        .jb_pin1_t(jb_pin1_t),
+        .jb_pin2_i(jb_pin2_i),
+        .jb_pin2_o(jb_pin2_o),
+        .jb_pin2_t(jb_pin2_t),
+        .jb_pin3_i(jb_pin3_i),
+        .jb_pin3_o(jb_pin3_o),
+        .jb_pin3_t(jb_pin3_t),
+        .jb_pin4_i(jb_pin4_i),
+        .jb_pin4_o(jb_pin4_o),
+        .jb_pin4_t(jb_pin4_t),
+        .jb_pin7_i(jb_pin7_i),
+        .jb_pin7_o(jb_pin7_o),
+        .jb_pin7_t(jb_pin7_t),
+        .jb_pin8_i(jb_pin8_i),
+        .jb_pin8_o(jb_pin8_o),
+        .jb_pin8_t(jb_pin8_t),
+        .jb_pin9_i(jb_pin9_i),
+        .jb_pin9_o(jb_pin9_o),
+        .jb_pin9_t(jb_pin9_t),
+        .jc_pin10_i(jc_pin10_i),
+        .jc_pin10_o(jc_pin10_o),
+        .jc_pin10_t(jc_pin10_t),
+        .jc_pin1_i(jc_pin1_i),
+        .jc_pin1_o(jc_pin1_o),
+        .jc_pin1_t(jc_pin1_t),
+        .jc_pin2_i(jc_pin2_i),
+        .jc_pin2_o(jc_pin2_o),
+        .jc_pin2_t(jc_pin2_t),
+        .jc_pin3_i(jc_pin3_i),
+        .jc_pin3_o(jc_pin3_o),
+        .jc_pin3_t(jc_pin3_t),
+        .jc_pin4_i(jc_pin4_i),
+        .jc_pin4_o(jc_pin4_o),
+        .jc_pin4_t(jc_pin4_t),
+        .jc_pin7_i(jc_pin7_i),
+        .jc_pin7_o(jc_pin7_o),
+        .jc_pin7_t(jc_pin7_t),
+        .jc_pin8_i(jc_pin8_i),
+        .jc_pin8_o(jc_pin8_o),
+        .jc_pin8_t(jc_pin8_t),
+        .jc_pin9_i(jc_pin9_i),
+        .jc_pin9_o(jc_pin9_o),
+        .jc_pin9_t(jc_pin9_t),
+        .leds_4bits_tri_i({leds_4bits_tri_i_3,leds_4bits_tri_i_2,leds_4bits_tri_i_1,leds_4bits_tri_i_0}),
+        .leds_4bits_tri_o({leds_4bits_tri_o_3,leds_4bits_tri_o_2,leds_4bits_tri_o_1,leds_4bits_tri_o_0}),
+        .leds_4bits_tri_t({leds_4bits_tri_t_3,leds_4bits_tri_t_2,leds_4bits_tri_t_1,leds_4bits_tri_t_0}),
+        .sws_4bits_tri_i(sws_4bits_tri_i));
+  IOBUF ja_pin10_iobuf
+       (.I(ja_pin10_o),
+        .IO(ja_pin10_io),
+        .O(ja_pin10_i),
+        .T(ja_pin10_t));
+  IOBUF ja_pin1_iobuf
+       (.I(ja_pin1_o),
+        .IO(ja_pin1_io),
+        .O(ja_pin1_i),
+        .T(ja_pin1_t));
+  IOBUF ja_pin2_iobuf
+       (.I(ja_pin2_o),
+        .IO(ja_pin2_io),
+        .O(ja_pin2_i),
+        .T(ja_pin2_t));
+  IOBUF ja_pin3_iobuf
+       (.I(ja_pin3_o),
+        .IO(ja_pin3_io),
+        .O(ja_pin3_i),
+        .T(ja_pin3_t));
+  IOBUF ja_pin4_iobuf
+       (.I(ja_pin4_o),
+        .IO(ja_pin4_io),
+        .O(ja_pin4_i),
+        .T(ja_pin4_t));
+  IOBUF ja_pin7_iobuf
+       (.I(ja_pin7_o),
+        .IO(ja_pin7_io),
+        .O(ja_pin7_i),
+        .T(ja_pin7_t));
+  IOBUF ja_pin8_iobuf
+       (.I(ja_pin8_o),
+        .IO(ja_pin8_io),
+        .O(ja_pin8_i),
+        .T(ja_pin8_t));
+  IOBUF ja_pin9_iobuf
+       (.I(ja_pin9_o),
+        .IO(ja_pin9_io),
+        .O(ja_pin9_i),
+        .T(ja_pin9_t));
+  IOBUF jb_pin10_iobuf
+       (.I(jb_pin10_o),
+        .IO(jb_pin10_io),
+        .O(jb_pin10_i),
+        .T(jb_pin10_t));
+  IOBUF jb_pin1_iobuf
+       (.I(jb_pin1_o),
+        .IO(jb_pin1_io),
+        .O(jb_pin1_i),
+        .T(jb_pin1_t));
+  IOBUF jb_pin2_iobuf
+       (.I(jb_pin2_o),
+        .IO(jb_pin2_io),
+        .O(jb_pin2_i),
+        .T(jb_pin2_t));
+  IOBUF jb_pin3_iobuf
+       (.I(jb_pin3_o),
+        .IO(jb_pin3_io),
+        .O(jb_pin3_i),
+        .T(jb_pin3_t));
+  IOBUF jb_pin4_iobuf
+       (.I(jb_pin4_o),
+        .IO(jb_pin4_io),
+        .O(jb_pin4_i),
+        .T(jb_pin4_t));
+  IOBUF jb_pin7_iobuf
+       (.I(jb_pin7_o),
+        .IO(jb_pin7_io),
+        .O(jb_pin7_i),
+        .T(jb_pin7_t));
+  IOBUF jb_pin8_iobuf
+       (.I(jb_pin8_o),
+        .IO(jb_pin8_io),
+        .O(jb_pin8_i),
+        .T(jb_pin8_t));
+  IOBUF jb_pin9_iobuf
+       (.I(jb_pin9_o),
+        .IO(jb_pin9_io),
+        .O(jb_pin9_i),
+        .T(jb_pin9_t));
+  IOBUF jc_pin10_iobuf
+       (.I(jc_pin10_o),
+        .IO(jc_pin10_io),
+        .O(jc_pin10_i),
+        .T(jc_pin10_t));
+  IOBUF jc_pin1_iobuf
+       (.I(jc_pin1_o),
+        .IO(jc_pin1_io),
+        .O(jc_pin1_i),
+        .T(jc_pin1_t));
+  IOBUF jc_pin2_iobuf
+       (.I(jc_pin2_o),
+        .IO(jc_pin2_io),
+        .O(jc_pin2_i),
+        .T(jc_pin2_t));
+  IOBUF jc_pin3_iobuf
+       (.I(jc_pin3_o),
+        .IO(jc_pin3_io),
+        .O(jc_pin3_i),
+        .T(jc_pin3_t));
+  IOBUF jc_pin4_iobuf
+       (.I(jc_pin4_o),
+        .IO(jc_pin4_io),
+        .O(jc_pin4_i),
+        .T(jc_pin4_t));
+  IOBUF jc_pin7_iobuf
+       (.I(jc_pin7_o),
+        .IO(jc_pin7_io),
+        .O(jc_pin7_i),
+        .T(jc_pin7_t));
+  IOBUF jc_pin8_iobuf
+       (.I(jc_pin8_o),
+        .IO(jc_pin8_io),
+        .O(jc_pin8_i),
+        .T(jc_pin8_t));
+  IOBUF jc_pin9_iobuf
+       (.I(jc_pin9_o),
+        .IO(jc_pin9_io),
+        .O(jc_pin9_i),
+        .T(jc_pin9_t));
+  IOBUF leds_4bits_tri_iobuf_0
+       (.I(leds_4bits_tri_o_0),
+        .IO(leds_4bits_tri_io[0]),
+        .O(leds_4bits_tri_i_0),
+        .T(leds_4bits_tri_t_0));
+  IOBUF leds_4bits_tri_iobuf_1
+       (.I(leds_4bits_tri_o_1),
+        .IO(leds_4bits_tri_io[1]),
+        .O(leds_4bits_tri_i_1),
+        .T(leds_4bits_tri_t_1));
+  IOBUF leds_4bits_tri_iobuf_2
+       (.I(leds_4bits_tri_o_2),
+        .IO(leds_4bits_tri_io[2]),
+        .O(leds_4bits_tri_i_2),
+        .T(leds_4bits_tri_t_2));
+  IOBUF leds_4bits_tri_iobuf_3
+       (.I(leds_4bits_tri_o_3),
+        .IO(leds_4bits_tri_io[3]),
+        .O(leds_4bits_tri_i_3),
+        .T(leds_4bits_tri_t_3));
+endmodule
